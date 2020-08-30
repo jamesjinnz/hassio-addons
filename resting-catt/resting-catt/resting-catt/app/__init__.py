a980a6f2b86fbce3f964270d5d821164108d1efc
Carl Bäckström <genzorg@gmail.com>
2020-08-30 14:48:32 +0200

    Improved error handling

--- resting-catt/resting-catt/resting-catt/app/__init__.py
+++ resting-catt/resting-catt/resting-catt/app/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, abort, jsonify, make_response
 from catt.controllers import setup_cast
+from catt.error import CastError
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def root():
@@ -9,16 +11,38 @@ def root():
         "msg": "Hello, there!"
     }
 
+
+@app.errorhandler(422)
+def unprocessable_entity(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.errorhandler(500)
+def unprocessable_entity(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
 @app.route("/cast_site", methods=['POST'])
 def cast_site():
     data = request.json
-    print(data)
-    if data['device'] and data['url']:
-        cst = setup_cast(data['device'], 
-                         controller="dashcast", 
-                         action="load_url", 
-                         prep="app")
-        cst.load_url(data['url'])
+    if all(key in data for key in ('device', 'url')):
+        try:
+            cst = setup_cast(data['device'],
+                             controller="dashcast",
+                             action="load_url",
+                             prep="app")
+            cst.load_url(data['url'])
+            return '', 204
+        except CastError as e:
+            abort(500, e)
+
+    else:
+        abort(422, 'device and url is required in post data')
+
 
 def run():
-    app.run(port=9898, host= '0.0.0.0')
+    app.run(port=9898, host='0.0.0.0')
